ae57e54540bbd70b3a0dbcf35500dbee2b4849ea
Ria <ria.jacob@outlook.com>
2022-04-07 12:59:33 +0400

    first commit

---
+++ server/create-lb-tables.js
@@ -0,0 +1,10 @@
+module.exports = function(app) {
+var ds = app.dataSources.postgres;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
+};
+
